c8a8dd2ef3d8ba8db5709432d6bb454a9bac3014
Thiago Lira <thiago.slira@outlook.com>
2023-07-05 14:54:39 -0300

    <feat>: Building the method to Logout, checking if there is an account logged. If is not, an error is launched

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -2,97 +2,110 @@
 
 public class Trybank
 {
-    public bool Logged;
-    public int loggedUser;
-    
-    //0 -> Número da conta
-    //1 -> Agência
-    //2 -> Senha
-    //3 -> Saldo
-    public int[,] Bank;
-    public int registeredAccounts;
-    private int maxAccounts = 50;
-    public Trybank()
+  public bool Logged;
+  public int loggedUser;
+
+  //0 -> Número da conta
+  //1 -> Agência
+  //2 -> Senha
+  //3 -> Saldo
+  public int[,] Bank;
+  public int registeredAccounts;
+  private int maxAccounts = 50;
+  public Trybank()
+  {
+    loggedUser = -99;
+    registeredAccounts = 0;
+    Logged = false;
+    Bank = new int[maxAccounts, 4];
+  }
+
+  // 1. Construa a funcionalidade de cadastrar novas contas
+  public void RegisterAccount(int number, int agency, int pass)
+  {
+    for (int i = 0; i < maxAccounts; i += 1)
     {
-        loggedUser = -99;
-        registeredAccounts = 0;
-        Logged = false;
-        Bank = new int[maxAccounts, 4];
+      if (Bank[i, 0] == number && Bank[i, 1] == agency)
+      {
+        throw new ArgumentException("A conta já está sendo usada!");
+      }
     }
 
-    // 1. Construa a funcionalidade de cadastrar novas contas
-    public void RegisterAccount(int number, int agency, int pass)
+    if (registeredAccounts >= maxAccounts)
     {
-        for (int i = 0; i < maxAccounts; i += 1) {
-            if (Bank[i, 0] == number && Bank[i, 1] == agency) {
-                throw new ArgumentException("A conta já está sendo usada!");
-            }
+      throw new ArgumentException("O banco está cheio!");
+    }
+
+    Bank[registeredAccounts, 0] = number;
+    Bank[registeredAccounts, 1] = agency;
+    Bank[registeredAccounts, 2] = pass;
+    Bank[registeredAccounts, 3] = 0;
+    registeredAccounts += 1;
+  }
+
+  // 2. Construa a funcionalidade de fazer Login
+  public void Login(int number, int agency, int pass)
+  {
+    if (loggedUser >= 0)
+    {
+      throw new AccessViolationException("Usuário já está logado");
+    }
+
+    for (int i = 0; i < maxAccounts; i += 1)
+    {
+      if (Bank[i, 0] == number && Bank[i, 1] == agency)
+      {
+        if (Bank[i, 2] == pass)
+        {
+          loggedUser = i;
+          Logged = true;
+          break;
         }
-
-        if (registeredAccounts >= maxAccounts) {
-            throw new ArgumentException("O banco está cheio!");
+        else
+        {
+          throw new ArgumentException("Senha incorreta");
         }
-
-        Bank[registeredAccounts, 0] = number;
-        Bank[registeredAccounts, 1] = agency;
-        Bank[registeredAccounts, 2] = pass;
-        Bank[registeredAccounts, 3] = 0;
-        registeredAccounts += 1;
+      }
     }
 
-    // 2. Construa a funcionalidade de fazer Login
-    public void Login(int number, int agency, int pass)
+    if (!Logged)
     {
-        if (loggedUser >= 0) {
-            throw new AccessViolationException("Usuário já está logado");
-        }
-
-        for (int i = 0; i < maxAccounts; i += 1) {
-            if (Bank[i, 0] == number && Bank[i, 1] == agency) {
-                if (Bank[i, 2] == pass) {
-                    loggedUser = i;
-                    Logged = true;
-                    break;
-                } else {
-                    throw new ArgumentException("Senha incorreta");
-                }
-            }
-        }
-
-        if (!Logged) {
-            throw new ArgumentException("Agência + Conta não encontrada");
-        }
+      throw new ArgumentException("Agência + Conta não encontrada");
     }
+  }
 
-    // 3. Construa a funcionalidade de fazer Logout
-    public void Logout()
+  // 3. Construa a funcionalidade de fazer Logout
+  public void Logout()
+  {
+    throw new NotImplementedException();
+  }
+
+  // 4. Construa a funcionalidade de checar o saldo
+  public int CheckBalance()
+  {
+    if (!Logged)
     {
-        throw new NotImplementedException();
+      throw new AccessViolationException("Usuário não está logado");
     }
+  }
 
-    // 4. Construa a funcionalidade de checar o saldo
-    public int CheckBalance()
-    {
-        throw new NotImplementedException();   
-    }
+  // 5. Construa a funcionalidade de depositar dinheiro
+  public void Deposit(int value)
+  {
+    throw new NotImplementedException();
+  }
 
-    // 5. Construa a funcionalidade de depositar dinheiro
-    public void Deposit(int value)
-    {
-        throw new NotImplementedException();
-    }
+  // 6. Construa a funcionalidade de sacar dinheiro
+  public void Withdraw(int value)
+  {
+    throw new NotImplementedException();
+  }
 
-    // 6. Construa a funcionalidade de sacar dinheiro
-    public void Withdraw(int value)
-    {
-        throw new NotImplementedException();
-    }
+  // 7. Construa a funcionalidade de transferir dinheiro entre contas
+  public void Transfer(int destinationNumber, int destinationAgency, int value)
+  {
+    throw new NotImplementedException();
+  }
 
-    // 7. Construa a funcionalidade de transferir dinheiro entre contas
-    public void Transfer(int destinationNumber, int destinationAgency, int value)
-    {
-        throw new NotImplementedException();
-    }
 
-   
 }
